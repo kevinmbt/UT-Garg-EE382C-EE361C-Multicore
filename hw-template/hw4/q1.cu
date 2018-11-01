7388f84802c845f8b7c52b408060d87f2723315f
kevinmbt <kperilla@utexas.edu>
2018-11-01 14:11:46 -0500

    q1a file write

--- hw-template/hw4/q1.cu
+++ hw-template/hw4/q1.cu
@@ -74,6 +74,11 @@ void a(vector<int> arr, int len) {
         min = b;
     }
 
+    FILE * fp;
+
+    fprintf (fp, "Minimum: %d", min);
+
+    fclose (fp);
     cout<<"Minimum: " << min << endl;
 
     cudaFree(d_arr); cudaFree(d_i);
